bf3a2ea844c86c4805edffed74f7bd9d418e598b
Pankoj-Dakua <pankoj6@gmail.com>
2026-05-27 18:23:58 +0600

    Change name

--- JS/javascript.js
+++ JS/javascript.js
@@ -14,6 +14,25 @@ function hideSidebar(){
 // End jascript for navbar.css
 
 
+// Start JavaScript for Sidenavbar
+    $('.btn').click(function(){
+        $(this).toggleClass("click");
+        $('.sidebar').toggleClass("show");
+    });
+    $('.feat-btn').click(function(){
+        $('nav ul .feat-show').toggleClass("show");
+        $('nav ul .first').toggleClass("rotate");
+    });
+    $('.serv-btn').click(function(){
+        $('nav ul .serv-show').toggleClass("show1");
+        $('nav ul .second').toggleClass("rotate");
+    });
+    $('nave ul li').click(function(){
+        $(this).addClass("active").siblings().removeClass("active");
+    });
+
+// End JavaScript for Sidenavbar
+
 
 
 
