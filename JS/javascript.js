8dc8f67dbd30f7cb5270b41355f4512c6a1b7f97
Pankoj-Dakua <pankoj6@gmail.com>
2026-05-29 13:20:22 +0600

    Change files

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,37 +1,50 @@
 
 // Start jascript for navbar.css
 
+
 //For hide or shwo menu bar
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display='flex'
-}
-function hideSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display='none'
-}
+// function showSidebar(){
+//     const sidebar = document.querySelector('.sidebar')
+//     sidebar.style.display='flex'
+// }
+// function hideSidebar(){
+//     const sidebar = document.querySelector('.sidebar')
+//     sidebar.style.display='none'
+// }
 
 // End jascript for navbar.css
 
 
 // Start JavaScript for Sidenavbar
-    $('.btn').click(function(){
-        $(this).toggleClass("click");
-        $('.sidebar').toggleClass("show");
+document.querySelector('.btn').addEventListener('click', function () {
+    this.classList.toggle('click');
+    document.querySelector('.sidebar').classList.toggle('show');
+});
+
+document.querySelector('.feat-btn').addEventListener('click', function () {
+    document.querySelector('nav ul .feat-show').classList.toggle('show');
+    document.querySelector('nav ul .first').classList.toggle('rotate');
+});
+
+document.querySelector('.serv-btn').addEventListener('click', function () {
+    document.querySelector('nav ul .serv-show').classList.toggle('show1');
+    document.querySelector('nav ul .second').classList.toggle('rotate');
+});
+
+document.querySelectorAll('nav ul li').forEach(function (item) {
+    item.addEventListener('click', function () {
+        document.querySelectorAll('nav ul li').forEach(function (li) {
+            li.classList.remove('active');
+        });
+
+        this.classList.add('active');
     });
-    $('.feat-btn').click(function(){
-        $('nav ul .feat-show').toggleClass("show");
-        $('nav ul .first').toggleClass("rotate");
-    });
-    $('.serv-btn').click(function(){
-        $('nav ul .serv-show').toggleClass("show1");
-        $('nav ul .second').toggleClass("rotate");
-    });
-    $('nave ul li').click(function(){
-        $(this).addClass("active").siblings().removeClass("active");
-    });
+});
 
 // End JavaScript for Sidenavbar
+
+
+
 
 
 
